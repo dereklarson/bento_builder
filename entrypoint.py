35e1a2469a5ebb358a33bfa430fe9c72426364eb
Derek Larson <larson.derek.a@gmail.com>
2020-07-28 12:42:09 -0500

    updates the logger utility, entrypoint for bento 0.0.2

--- entrypoint.py
+++ entrypoint.py
@@ -4,14 +4,13 @@ from bento import bento
 from bento.common.structure import ENV
 from bento.common import logger
 
-logging = logger.fancy_logger(__name__)
+logging = logger.fancy_logger("entrypoint")
 
 # This generates the app from the descriptor
 logging.info(f"Loading descriptor from {ENV.APP}/descriptor.py")
 desc_file = importlib.import_module(f"{ENV.APP}.descriptor")
 app_def = bento.Bento(desc_file.descriptor)
-app_def.write_css()
-app_def.write("bento_app.py")
+app_def.write(app_output="bento_app.py")
 
 # Gunicorn requires an object containing the server: app.server
 from bento_app import app  # noqa
